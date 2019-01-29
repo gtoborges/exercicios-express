c3b55a3bb63ad2a89ccf088a123a17a6b16308f9
gtoborges <gtoborges@gmail.com>
2019-01-29 17:06:34 -0200

    implementando funcoes de teste em index.js e usando postman

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const saudacao = require('./saudacaoMid')
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    console.log('Antes')
+    next()
+})
+
+app.use(saudacao('Augusto'))
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo ${req.query.completo} ano =${req.query.ano}`)
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
+
+app.post('/corpo', (req, res) =>{
+    res.send(req.body.nome3)
+    // let corpo =''
+    // req.on('data', function(parte){
+    //     corpo += parte
+    // })
+
+    // req.on('end', function(){
+    //     res.send(corpo)
+    // })
+})
+
+app.get('/opa', (req, res, next) => {
+    console.log('Durante')
+    res.json({
+        data: [
+            {id: 1, name: 'Marcelo', position: 1},
+            {id: 2, name: 'Vitor', position: 2},
+            {id:3, name: 'Augusto', position: 3}
+        ],
+        count: 3,
+        limit: 10,
+    })
+    next()
+})
+
+app.use((req, res) => {
+    console.log('Depois')
+})
 
 app.listen(3000, () => {
     console.log('Backend executando...')
